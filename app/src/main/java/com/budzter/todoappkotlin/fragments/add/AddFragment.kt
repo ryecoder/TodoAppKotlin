3581eb991a9fd228b5fe676213ba01e53734462e
Ryanell Maniego <28801036+ryecoder@users.noreply.github.com>
2020-09-28 14:45:16 +0800

    Finished Navigation Lesson

--- app/src/main/java/com/budzter/todoappkotlin/fragments/add/AddFragment.kt
+++ app/src/main/java/com/budzter/todoappkotlin/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.budzter.todoappkotlin.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.budzter.todoappkotlin.R
 
 class AddFragment : Fragment() {
@@ -14,9 +12,15 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Menu
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
-
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
 }
--- app/src/main/java/com/budzter/todoappkotlin/fragments/list/ListFragment.kt
+++ app/src/main/java/com/budzter/todoappkotlin/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.budzter.todoappkotlin.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.budzter.todoappkotlin.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -24,7 +22,17 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        view.list_layout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        // Menu
+        setHasOptionsMenu(true)
+
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
 }
